3d52fc9d606872ad5a425c0b8b7ee8e8571554e6
prashant mane <maneprashant096@gmail.com>
2024-08-06 20:33:43 +0530

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppASPCoreWepAPI.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string firstName { get; set; }
+
+        [Required]
+        public string lastName { get; set; }
+
+        [Required]
+        public int age { get; set; }
+
+        [Required]
+        public string email { get; set; }
+    }
+
+}
+
